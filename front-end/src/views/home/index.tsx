11c0fe84ceda46fbf0754359d7e2e767b0ea0db5
unknown <alexandre8603@gmail.com>
2022-06-01 13:55:32 -0300

    instala fontawesome

--- front-end/src/views/home/index.tsx
+++ front-end/src/views/home/index.tsx
@@ -1,3 +1,5 @@
+import { faFacebookMessenger } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "react-bootstrap";
 import { toast } from "react-toastify";
 import styled from "styled-components";
@@ -6,6 +8,7 @@ export function Home () {
     return (
     <div>
       <Tittle onClick={() => toast.success('oi')}>oi</Tittle>
+      <FontAwesomeIcon icon={faFacebookMessenger} />
     </div>
     )
 }
