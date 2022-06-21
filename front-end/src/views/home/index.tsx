07df2dd054c7efad482c20c85c17b280ac8064a5
unknown <alexandre8603@gmail.com>
2022-06-21 17:55:22 -0300

    altera css home

--- front-end/src/views/home/index.tsx
+++ front-end/src/views/home/index.tsx
@@ -7,15 +7,14 @@ import { selectUserLoggedIn } from "../../store/slices/userSlice"
 import terraviva from "../../assetes/img/terraviva.jpg"
 import terramorta from "../../assetes/img/poluicao.jpg"
 import { useEffect, useState } from "react"
-import { Button } from "react-bootstrap"
 
 type Props = {
-  startTransparent?: boolean
+  worldAnimation?: boolean
   darkmode: boolean
   getvaluedarkmode: string
 }
 
-export function Home ({startTransparent = true , darkmode = true}: Props) {
+export function Home ({worldAnimation = true , darkmode = true}: Props) {
     const [isdarkmode, setDarkmode] = useState(darkmode)
     const buttonHandler = () => {
       setDarkmode(darkmode => !darkmode)
@@ -23,12 +22,12 @@ export function Home ({startTransparent = true , darkmode = true}: Props) {
     }
     const getvaluedarkmode = localStorage.getItem('value')
     const getvaluedarklight = JSON.parse(getvaluedarkmode || '{}') 
-    const [isTransparent, setIsTransparent] = useState(startTransparent)
+    const [isWorldAnimation, setWorldAnimation] = useState(worldAnimation)
     useEffect(() => {
         const scrollChange = () => {
           const isLowScroll = window.scrollY < 100
-          if (startTransparent && isLowScroll !== isTransparent) {
-            setIsTransparent(isLowScroll)
+          if (worldAnimation && isLowScroll !== isWorldAnimation) {
+            setWorldAnimation(isLowScroll)
             console.log(isLowScroll)
           }
         }
@@ -36,11 +35,23 @@ export function Home ({startTransparent = true , darkmode = true}: Props) {
         return () => {
           window.removeEventListener('scroll', scrollChange)
         }
-      }, [isTransparent, startTransparent])
-  const isUserLoggedIn = useSelector(selectUserLoggedIn)
+      }, [isWorldAnimation, worldAnimation])
+    const isUserLoggedIn = useSelector(selectUserLoggedIn)
     return (
     <main className={!getvaluedarklight ? 'bg-dark' : 'bg-white'}>
-      <Button onClick={buttonHandler}>oi </Button>
+      <div className="d-flex justify-content-lg-between" >
+        <button onClick={buttonHandler} className='bg-secondary align-itens-center'>Dark/Light</button>
+        {isUserLoggedIn ? (
+          <div className="d-flex justify-content-center gap-3 d-none d-lg-block">
+            <CustomButton loading size='lg' variant="success" to="/novacarona">Nova carona</CustomButton>   
+          </div>
+        ) : (
+          <div className="d-flex justify-content-center gap-3 d-none d-lg-block">
+            <CustomButton size='sm' variant="success" to="/login">ENTRAR</CustomButton>
+            <CustomButton size='sm' className="ms-3" variant="success" to="/signin">CRIAR CONTA</CustomButton>
+          </div>
+      )}
+      </div>
       <div className="d-flex justify-content-center flex-column">
         <div className="bg-success mt-5 mb-1 ms-5 me-5 fw-bold">
           <Ppad className="mt-5 text-white bold mb-5 h2">THINK CLEAN.<br/>RIDE TOGETHER.</Ppad>
@@ -50,17 +61,17 @@ export function Home ({startTransparent = true , darkmode = true}: Props) {
       <div className={!getvaluedarklight ? 'bg-white mt-3 mb-3 ms-5 me-5' : 'bg-dark mt-3 mb-3 ms-5 me-5'}>
           <p className={!getvaluedarklight ? 'text-dark bold text-center ps-0' : 'text-white bold text-center ps-0'}>AND SAVE THE PLANET.</p>
       </div>
-      <div className="d-none d-md-flex justify-content-center  ms-5 me-5">
-        <img src={terraviva} alt="terraviva" className="img-fluid m-0" />
-        <Imganimation className={isTransparent ? 'd-none' : 'd-block' && 'd-md-none d-lg-block bg-secondary mb-0'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci omnis consequuntur doloremque accusamus voluptates, nemo similique deserunt inventore! Facilis ipsum neque nisi temporibus quia ipsam accusamus dicta esse harum repudiandae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum rem et autem ab eos architecto sint deleniti iure. Maiores doloremque molestiae saepe nulla vitae dignissimos officia quaerat perspiciatis numquam harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maiores, quia quasi culpa doloribus rem vero! Hic cupiditate accusantium ullam corporis in iure quam. Vero dicta aliquid deleniti iste obcaecati!</Imganimation>
-        <img src={terramorta}  alt="terraviva" className="img-fluid" />
-      </div>
+      <Imganimation className="d-none d-md-flex justify-content-center  ms-5 me-5 ss">
+        <img src={terraviva} alt="terraviva" className="img-fluid m-0 " />
+        <Parag className={isWorldAnimation ? 'd-none' : 'd-block' && 'd-md-none d-lg-block bg-secondary mb-0 as'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci omnis consequuntur doloremque accusamus voluptates, nemo similique deserunt inventore! Facilis ipsum neque nisi temporibus quia ipsam accusamus dicta esse harum repudiandae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum rem et autem ab eos architecto sint deleniti iure. Maiores doloremque molestiae saepe nulla vitae dignissimos officia quaerat perspiciatis numquam harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maiores, quia quasi culpa doloribus rem vero! Hic cupiditate accusantium ullam corporis in iure quam. Vero dicta aliquid deleniti iste obcaecati!</Parag>
+        <img src={terramorta}  alt="terraviva" className="img-fluid " />
+      </Imganimation>
       {isUserLoggedIn ? (
-        <div className="d-flex justify-content-center gap-3">
+        <div className="d-flex justify-content-center gap-3 d-lg-none">
         <CustomButton loading size='lg' variant="success" to="/novacarona">Nova carona</CustomButton>   
       </div>
       ) : (
-      <div className="d-flex justify-content-center gap-3">
+      <div className="d-flex justify-content-center gap-3 d-lg-none">
         <CustomButton loading size='lg' variant="success" to="/login">ENTRAR</CustomButton>
         <CustomButton size='lg' variant="success" to="/signin">CRIAR CONTA</CustomButton>
       </div>
@@ -131,7 +142,14 @@ const Ppad = styled.div`
 text-align: center;
 vertical-align: middle;
 `
-
-const Imganimation = styled.p`
-  transition: all .3s linear;
+const Parag = styled.p`
+ 
 `
+const Imganimation = styled.div`
+ &:hover {
+    ${Parag} {
+    display: block;
+  }
+ }
+`
+
