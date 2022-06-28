5f2081e49e22b4aad592ca420d5caf95c9d3e450
XandaoTP <alexandre8603@gmail.com>
2022-06-28 14:37:04 -0300

    ajusta css home

--- front-end/src/views/home/index.tsx
+++ front-end/src/views/home/index.tsx
@@ -7,6 +7,7 @@ import { selectUserLoggedIn } from "../../store/slices/userSlice"
 import terraviva from "../../assetes/img/terraviva.jpg"
 import terramorta from "../../assetes/img/poluicao.jpg"
 import { useEffect, useState } from "react"
+import { Card, Col, Container, Row } from "react-bootstrap"
 
 type Props = {
   worldAnimation?: boolean
@@ -38,12 +39,13 @@ export function Home ({worldAnimation = true , darkmode = true}: Props) {
       }, [isWorldAnimation, worldAnimation])
     const isUserLoggedIn = useSelector(selectUserLoggedIn)
     return (
-    <main className={!getvaluedarklight ? 'bg-dark' : 'bg-white'}>
+    <MainStyled>
+      <ContainerStyled fluid="sm" className={!getvaluedarklight ? 'bg-dark' : 'bg-white flex-1'}>
       <div className="d-flex justify-content-lg-between" >
         <button onClick={buttonHandler} className='bg-secondary align-itens-center'>Dark/Light</button>
         {isUserLoggedIn ? (
           <div className="d-flex justify-content-center gap-3 d-none d-lg-block">
-            <CustomButton loading size='lg' variant="success" to="/novacarona">Nova carona</CustomButton>   
+            <CustomButton loading size='lg' variant="success" to="/novacarona"className="mt-3">Nova carona</CustomButton>   
           </div>
         ) : (
           <div className="d-flex justify-content-center gap-3 d-none d-lg-block">
@@ -54,7 +56,7 @@ export function Home ({worldAnimation = true , darkmode = true}: Props) {
       </div>
       <div className="d-flex justify-content-center flex-column">
         <div className="bg-success mt-5 mb-1 ms-5 me-5 fw-bold">
-          <Ppad className="mt-5 text-white bold mb-5 h2">THINK CLEAN.<br/>RIDE TOGETHER.</Ppad>
+          <Ppad className="mt-5 text-white bold mb-5 h2">ECO TAXI<br/>THINK CLEAN.<br/>RIDE TOGETHER.</Ppad>
         </div>
         <img src={Logo} alt='mundo' width="258px" height='230px'className="d-md-none align-self-center mt-4"/>
       </div>
@@ -63,7 +65,7 @@ export function Home ({worldAnimation = true , darkmode = true}: Props) {
       </div>
       <Imganimation className="d-none d-md-flex justify-content-center  ms-5 me-5 ss">
         <img src={terraviva} alt="terraviva" className="img-fluid m-0 " />
-        <Parag className={isWorldAnimation ? 'd-none' : 'd-block' && 'd-md-none d-lg-block bg-secondary mb-0 as'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci omnis consequuntur doloremque accusamus voluptates, nemo similique deserunt inventore! Facilis ipsum neque nisi temporibus quia ipsam accusamus dicta esse harum repudiandae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum rem et autem ab eos architecto sint deleniti iure. Maiores doloremque molestiae saepe nulla vitae dignissimos officia quaerat perspiciatis numquam harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maiores, quia quasi culpa doloribus rem vero! Hic cupiditate accusantium ullam corporis in iure quam. Vero dicta aliquid deleniti iste obcaecati!</Parag>
+        <Parag className={isWorldAnimation ? 'd-none parag' : 'd-block' && 'd-md-none d-lg-block bg-info mb-0 as'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci omnis consequuntur doloremque accusamus voluptates, nemo similique deserunt inventore! Facilis ipsum neque nisi temporibus quia ipsam accusamus dicta esse harum repudiandae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum rem et autem ab eos architecto sint deleniti iure. Maiores doloremque molestiae saepe nulla vitae dignissimos officia quaerat perspiciatis numquam harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat maiores, quia quasi culpa doloribus rem vero! Hic cupiditate accusantium ullam corporis in iure quam. Vero dicta aliquid deleniti iste obcaecati!</Parag>
         <img src={terramorta}  alt="terraviva" className="img-fluid " />
       </Imganimation>
       {isUserLoggedIn ? (
@@ -76,65 +78,38 @@ export function Home ({worldAnimation = true , darkmode = true}: Props) {
         <CustomButton size='lg' variant="success" to="/signin">CRIAR CONTA</CustomButton>
       </div>
       )}
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
-      <p>a</p>
+      <Row>
+        <Col className="d-flex flex-row gap-2" xs={12}>
+      {[
+    'Primary',
+    'Secondary',
+    'Success',
+    'Danger',
+    'Warning',
+    'Light',
+  ].map((variant) => (
+        <CardStyled
+          bg={variant.toLowerCase()}
+          key={variant}
+          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '18rem' }}
+          className="my-2"    
+        >
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>{variant} Card Title </Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </CardStyled>
+  ))}
+  </Col>
+  </Row>
+      </ContainerStyled>
       <Footer withoutMargin />
-    </main>    
+    </MainStyled>    
     )
 }
 
@@ -152,4 +127,22 @@ const Imganimation = styled.div`
   }
  }
 `
-
+const MainStyled = styled.main`
+  background-color: rgb(25,135,84);
+  display: flex;
+  flex-direction: column;
+`
+const ContainerStyled = styled(Container)`
+  flex:1;
+` 
+const CardStyled = styled(Card)`
+  .parag:hover,
+  &:focus { 
+  transition: transform .2s;
+  transform: scale(1.2);
+  z-index: 10;
+  }
+  &:active {
+    color: red;
+  }
+`
