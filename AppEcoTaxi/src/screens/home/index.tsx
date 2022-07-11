8db531590fba3f9561cd5d856468d27cc1c4d461
XandaoTP <alexandre8603@gmail.com>
2022-07-11 14:35:59 -0300

    altera css

--- AppEcoTaxi/src/screens/home/index.tsx
+++ AppEcoTaxi/src/screens/home/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { CustomButton } from "../../assets/components/custombutton";
 import logo from '../../assets/img/mudanca-climatica.jpg'
 
 export function HomeScreen () {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.area}>
             <View style={styles.container}>
                 <Text 
                 style={styles.text}>
@@ -13,6 +14,7 @@ export function HomeScreen () {
                 <Image 
                 source={logo}
                 style={styles.logo}/>
+                <CustomButton>Entrar</CustomButton>
             </View>
         </SafeAreaView>
     )
@@ -20,12 +22,22 @@ export function HomeScreen () {
 }
 
 const styles = StyleSheet.create({
+    area: {
+        flex:1,
+        backgroundColor: '#232323',
+    },
     container: {
-        alignItems: 'center'
-        },
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        flex: 1,
+    },
     text: {
-        color: '#00a95d',
-        fontSize: 30
+        color: '#1f6920',
+        fontSize: 30,
+        textShadowColor: 'rgba(9, 131, 23, 0.25)',
+        textShadowOffset: {width: 0, height: 4},
+        textShadowRadius: 1,
     },
     logo: {
         width: 300,
